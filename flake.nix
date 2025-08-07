84ee53d15557059cdea6ea8966d4a788ad312557
Sojus07 <Sojus07>
2025-08-07 04:45:42 +0200

    kekl

--- flake.nix
+++ flake.nix
@@ -10,6 +10,10 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
+    packages.${system} = {
+      inherit pkgs;
+      modules = [ ./default.nix ];
+    }.nvim;
     
     nixosModules.default = { pkgs, lib, ... }: {
       imports = [
