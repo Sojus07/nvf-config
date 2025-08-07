a611b9f12aa4459bca7a63a1719ac936193687fb
Sojus07 <Sojus07>
2025-08-07 04:47:23 +0200

    kekl

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My autistic nvf config";
-
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
@@ -9,18 +9,16 @@
   outputs = { self, nixpkgs, nvf, ... }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system} = {
+    nvimWrapped = nvf.lib.neovimConfiguration {
       inherit pkgs;
       modules = [ ./default.nix ];
-    }.nvim;
-    
+    };
+  in {
+    packages.${system}.default = nvimWrapped.neovim;
     nixosModules.default = { pkgs, lib, ... }: {
-      imports = [
-        nvf.nixosModules.default
-      ];
-      
-      programs.nvf.settings = ./default.nix;
+      imports = [ nvf.nixosModules.default ];
+      programs.nvf = { enable = true; settings = import ./default.nix; };
     };
   };
 }
+
