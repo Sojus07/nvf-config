f7a3d58145ddafb6701cf4314a923b6b99a29f55
Sojus07 <Sojus07@0xcc01.de>
2025-08-12 23:30:41 +0200

    kekl

--- default.nix
+++ default.nix
@@ -23,24 +23,7 @@
     theme = {
       enable = true;
       name = "oxocarbon";
-      base16-colors = {
-        base00 = "#090a0a";
-        base01 = "#3C3E41";
-        base02 = "#403F40";
-        base03 = "#3F4043";
-        base04 = "#4A4B4F";
-        base05 = "#b6b7b7";
-        base06 = "#7f8080";
-        base07 = "#b6b7b7";
-        base08 = "#3C3E41";
-        base09 = "#403F40";
-        base0A = "#3F4043";
-        base0B = "#4A4B4F";
-        base0C = "#5E5F61";
-        base0D = "#69696A";
-        base0E = "#b6b7b7";
-        base0F = "#7f8080";
-      }; 
+      style = "dark";
     };
   };
 }
